ec64dc90a3669d62629efd23ffb2fae56c02e7ce
王一成 <wangyicheng2012ice@163.com>
2015-12-30 19:58:34 +0800

    修改

--- LoveLife/Model/HomeModel/HomeModel.m
+++ LoveLife/Model/HomeModel/HomeModel.m
@@ -13,6 +13,7 @@
 {
     if ([key isEqualToString:@"id"]) {
         self.dataID = value;
+        
     }
 }
 @end
--- LoveLife/viewController/ReadVC/ReadRootViewController.m
+++ LoveLife/viewController/ReadVC/ReadRootViewController.m
@@ -15,6 +15,7 @@
 @implementation ReadRootViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
